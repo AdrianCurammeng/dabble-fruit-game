34c7ff6e83abe25eae99c48229273c5913ce8e38
Adrian Curammeng <adrianvcurammeng@gmail.com>
2022-12-10 20:01:20 -0500

    Added functionality for bomb.js

--- bomb.js
+++ bomb.js
@@ -1,7 +1,46 @@
 class Bomb {
 
     constructor() {
-        
+        this.scle = 10;
+        this.size = 20;
+        this.x = (this.scle)*Math.round(random(0,40));
+        this.y = 10;
+        this.ySpeed = 30;
+    }
+
+    display() {
+
+        fill(0);
+        noStroke();
+        ellipse(this.x, this.y, this.size, this.size);
+
+    }
+
+    fall() {
+
+        this.y += this.ySpeed;
+
+        if(this.y > height + this.scle) {
+            this.reset();
+        }
+
+    }
+
+    score() {
+
+        if(this.x >= h.x && this.x <= h.x + h.scle*h.size && this.y + this.size/2 == h.y) {
+            this.reset();
+            score--;
+            console.log('OUCH');
+        }
+
+    }
+
+    reset() {
+
+        this.x = (this.scle)*Math.round(random(0,40));
+        this.y = 10;
+
     }
 
 }
